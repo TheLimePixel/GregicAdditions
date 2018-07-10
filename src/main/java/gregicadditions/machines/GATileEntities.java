44c5e0e4b0c5ebb803665e91c7b05a79e6297846
Bartz24 <bartz24games@gmail.com>
2018-07-10 11:43:44 -0400

    cluster mill

--- src/main/java/gregicadditions/machines/GATileEntities.java
+++ src/main/java/gregicadditions/machines/GATileEntities.java
@@ -7,12 +7,18 @@ import gregtech.api.render.Textures;
 
 public class GATileEntities {
     public static SimpleMachineMetaTileEntity[] CIRCUITASSEMBLER = new SimpleMachineMetaTileEntity[4];
+    public static SimpleMachineMetaTileEntity[] CLUSTERMILL = new SimpleMachineMetaTileEntity[4];
 
     public static void init() {
         CIRCUITASSEMBLER[0] = GregTechAPI.registerMetaTileEntity(2000, new SimpleMachineMetaTileEntity("circuit_assembler.lv", GARecipeMaps.CIRCUIT_ASSEMBLER_RECIPES, Textures.ASSEMBLER_OVERLAY, 1));
         CIRCUITASSEMBLER[1] = GregTechAPI.registerMetaTileEntity(2001, new SimpleMachineMetaTileEntity("circuit_assembler.mv", GARecipeMaps.CIRCUIT_ASSEMBLER_RECIPES, Textures.ASSEMBLER_OVERLAY, 2));
         CIRCUITASSEMBLER[2] = GregTechAPI.registerMetaTileEntity(2002, new SimpleMachineMetaTileEntity("circuit_assembler.hv", GARecipeMaps.CIRCUIT_ASSEMBLER_RECIPES, Textures.ASSEMBLER_OVERLAY, 3));
         CIRCUITASSEMBLER[3] = GregTechAPI.registerMetaTileEntity(2003, new SimpleMachineMetaTileEntity("circuit_assembler.ev", GARecipeMaps.CIRCUIT_ASSEMBLER_RECIPES, Textures.ASSEMBLER_OVERLAY, 4));
+
+        CLUSTERMILL[0] = GregTechAPI.registerMetaTileEntity(2004, new SimpleMachineMetaTileEntity("cluster_mill.lv", GARecipeMaps.CLUSTER_MILL_RECIPES, Textures.WIREMILL_OVERLAY, 1));
+        CLUSTERMILL[1] = GregTechAPI.registerMetaTileEntity(2005, new SimpleMachineMetaTileEntity("cluster_mill.mv", GARecipeMaps.CLUSTER_MILL_RECIPES, Textures.WIREMILL_OVERLAY, 2));
+        CLUSTERMILL[2] = GregTechAPI.registerMetaTileEntity(2006, new SimpleMachineMetaTileEntity("cluster_mill.hv", GARecipeMaps.CLUSTER_MILL_RECIPES, Textures.WIREMILL_OVERLAY, 3));
+        CLUSTERMILL[3] = GregTechAPI.registerMetaTileEntity(2007, new SimpleMachineMetaTileEntity("cluster_mill.ev", GARecipeMaps.CLUSTER_MILL_RECIPES, Textures.WIREMILL_OVERLAY, 4));
 
     }
 }
--- src/main/java/gregicadditions/recipes/GARecipeMaps.java
+++ src/main/java/gregicadditions/recipes/GARecipeMaps.java
@@ -5,13 +5,17 @@ import gregtech.api.gui.widgets.ProgressWidget;
 import gregtech.api.recipes.RecipeMap;
 import gregtech.api.recipes.builders.AssemblerRecipeBuilder;
 import gregtech.api.recipes.builders.IntCircuitRecipeBuilder;
+import gregtech.api.recipes.builders.SimpleRecipeBuilder;
 import stanhebben.zenscript.annotations.ZenProperty;
 
 public class GARecipeMaps {
     @ZenProperty
     public static final RecipeMap<IntCircuitRecipeBuilder> CIRCUIT_ASSEMBLER_RECIPES;
+    public static final RecipeMap<SimpleRecipeBuilder> CLUSTER_MILL_RECIPES;
 
-    static{
+    static {
         CIRCUIT_ASSEMBLER_RECIPES = (new RecipeMap("circuit_assembler", 1, 6, 1, 1, 0, 1, 0, 0, 1, new AssemblerRecipeBuilder())).setSlotOverlay(false, false, GuiTextures.CIRCUIT_OVERLAY).setProgressBar(GuiTextures.PROGRESS_BAR_CIRCUIT, ProgressWidget.MoveType.HORIZONTAL);
+        CLUSTER_MILL_RECIPES = (new RecipeMap("cluster_mill", 1, 1, 1, 1, 0, 0, 0, 0, 1, new SimpleRecipeBuilder())).setSlotOverlay(false, false, GuiTextures.BENDER_OVERLAY).setProgressBar(GuiTextures.PROGRESS_BAR_BENDING, ProgressWidget.MoveType.HORIZONTAL);
+
     }
 }
