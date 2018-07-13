f09e17bbc22a59af7dab0f5779111c2c736572a6
Bartz24 <bartz24games@gmail.com>
2018-07-13 18:13:55 -0400

    Added other removeRecipe methods

--- src/main/java/gregicadditions/recipes/GARecipeRemoval.java
+++ src/main/java/gregicadditions/recipes/GARecipeRemoval.java
@@ -8,6 +8,7 @@ import gregtech.api.unification.material.type.IngotMaterial;
 import gregtech.api.unification.material.type.Material;
 import gregtech.api.unification.ore.OrePrefix;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -30,5 +31,22 @@ public class GARecipeRemoval {
         for (ItemStack s : itemInputs)
             inputs.add(s);
         map.removeRecipe(map.findRecipe(Integer.MAX_VALUE, inputs, Collections.EMPTY_LIST));
+    }
+
+    private static void removeRecipesByInputs(RecipeMap map, FluidStack... fluidInputs) {
+        List<FluidStack> inputs = new ArrayList<>();
+        for (FluidStack s : fluidInputs)
+            inputs.add(s);
+        map.removeRecipe(map.findRecipe(Integer.MAX_VALUE, Collections.EMPTY_LIST, inputs));
+    }
+
+    private static void removeRecipesByInputs(RecipeMap map, ItemStack[] itemInputs, FluidStack[] fluidInputs) {
+        List<ItemStack> itemIn = new ArrayList<>();
+        for (ItemStack s : itemInputs)
+            itemIn.add(s);
+        List<FluidStack> fluidIn = new ArrayList<>();
+        for (FluidStack s : fluidInputs)
+            fluidIn.add(s);
+        map.removeRecipe(map.findRecipe(Integer.MAX_VALUE, itemIn, fluidIn));
     }
 }
