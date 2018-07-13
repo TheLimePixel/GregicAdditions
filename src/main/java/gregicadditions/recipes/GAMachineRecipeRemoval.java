c307541a5fd38e9a985928d090b4d0742387f9ef
Bartz24 <bartz24games@gmail.com>
2018-07-13 18:40:59 -0400

    Fixed old wrench recipe removal

--- src/main/java/gregicadditions/recipes/GAMachineRecipeRemoval.java
+++ src/main/java/gregicadditions/recipes/GAMachineRecipeRemoval.java
@@ -1,15 +1,18 @@
 package gregicadditions.recipes;
 
+import gregtech.api.recipes.ModHandler;
 import gregtech.api.recipes.RecipeMap;
 import gregtech.api.recipes.RecipeMaps;
 import gregtech.api.recipes.ingredients.IntCircuitIngredient;
 import gregtech.api.unification.OreDictUnifier;
 import gregtech.api.unification.material.Materials;
+import gregtech.api.unification.material.type.DustMaterial;
 import gregtech.api.unification.material.type.IngotMaterial;
 import gregtech.api.unification.material.type.Material;
 import gregtech.api.unification.ore.OrePrefix;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.FluidStack;
 
 import java.util.ArrayList;
@@ -26,20 +29,25 @@ public class GAMachineRecipeRemoval {
                     OreDictUnifier.get(OrePrefix.plate, m),
                     IntCircuitIngredient.getIntegratedCircuit(0));
             //Remove Old Rotor Recipe
-            if (!OreDictUnifier.get(OrePrefix.rotor,m).isEmpty())
+            if (!OreDictUnifier.get(OrePrefix.rotor, m).isEmpty())
                 removeRecipesByInputs(RecipeMaps.ASSEMBLER_RECIPES,
-                        OreDictUnifier.get(OrePrefix.plate,m,4),
-                        OreDictUnifier.get(OrePrefix.ring,m));
+                        OreDictUnifier.get(OrePrefix.plate, m, 4),
+                        OreDictUnifier.get(OrePrefix.ring, m));
+
+            //Remove old wrench recipes
+            if (m instanceof IngotMaterial && !m.hasFlag(DustMaterial.MatFlags.NO_SMASHING)) {
+                ModHandler.removeRecipeByName(new ResourceLocation(String.format("gregtech:wrench_%s", m.toString())));
+            }
         }
         //Remove Old Bucket Recipe
         removeRecipesByInputs(RecipeMaps.BENDER_RECIPES,
-                OreDictUnifier.get(OrePrefix.plate,Materials.Iron,12),
+                OreDictUnifier.get(OrePrefix.plate, Materials.Iron, 12),
                 IntCircuitIngredient.getIntegratedCircuit(1));
         removeRecipesByInputs(RecipeMaps.BENDER_RECIPES,
-                OreDictUnifier.get(OrePrefix.plate,Materials.WroughtIron,12),
+                OreDictUnifier.get(OrePrefix.plate, Materials.WroughtIron, 12),
                 IntCircuitIngredient.getIntegratedCircuit(1));
         //Fix Brick Exploit
-        removeRecipesByInputs(RecipeMaps.MACERATOR_RECIPES,new ItemStack(Items.BRICK));
+        removeRecipesByInputs(RecipeMaps.MACERATOR_RECIPES, new ItemStack(Items.BRICK));
     }
 
     private static void removeRecipesByInputs(RecipeMap map, ItemStack... itemInputs) {
