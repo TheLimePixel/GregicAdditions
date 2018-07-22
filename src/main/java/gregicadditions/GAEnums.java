7708dd3e93397f5df42f3f2dca809eeb37c57756
Bartz24 <bartz24games@gmail.com>
2018-07-22 14:34:11 -0400

    added circuitGA prefix

--- src/main/java/gregicadditions/GAEnums.java
+++ src/main/java/gregicadditions/GAEnums.java
@@ -46,6 +46,11 @@ public class GAEnums {
                 new Class[]{String.class, long.class, Material.class, MaterialIconType.class, long.class, Condition.class},
                 "Large Pipe", GTValues.M, null, MaterialIconType.valueOf("pipeGaLarge"), OrePrefix.Flags.ENABLE_UNIFICATION,
                 OrePrefix.and(instanceOfIngotMat(), OrePrefix.hasFlag(IngotMaterial.MatFlags.GENERATE_ROTOR)));
+
+        EnumHelper.addEnum(OrePrefix.class, "circuitGA",
+                new Class[]{String.class, long.class, Material.class, MaterialIconType.class, long.class, Condition.class},
+                "GA Circuits", -1L, null, null, OrePrefix.Flags.ENABLE_UNIFICATION | OrePrefix.Flags.DISALLOW_RECYCLING,
+                null);
     }
 
     public static Condition<Material> instanceOfDustMat() {
