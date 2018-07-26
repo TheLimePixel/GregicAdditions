14e640f63c7cc0cb3599505b4dca16a3e0426ae6
Bartz24 <bartz24games@gmail.com>
2018-07-26 12:03:31 -0400

    Added new distillation recipe map

--- src/main/java/gregicadditions/recipes/GARecipeMaps.java
+++ src/main/java/gregicadditions/recipes/GARecipeMaps.java
@@ -7,6 +7,7 @@ import gregtech.api.recipes.RecipeMap;
 import gregtech.api.recipes.builders.AssemblerRecipeBuilder;
 import gregtech.api.recipes.builders.IntCircuitRecipeBuilder;
 import gregtech.api.recipes.builders.SimpleRecipeBuilder;
+import gregtech.api.recipes.builders.UniversalDistillationRecipeBuilder;
 import stanhebben.zenscript.annotations.ZenProperty;
 
 import java.util.ArrayList;
@@ -18,11 +19,14 @@ public class GARecipeMaps {
     public static final RecipeMap<SimpleRecipeBuilder> CLUSTER_MILL_RECIPES;
     public static final RecipeMap<SimpleRecipeBuilder> ASSEMBLY_LINE_RECIPES;
     public static final List<CokeOvenRecipe> COKE_OVEN_RECIPES;
+    public static final RecipeMap<UniversalDistillationRecipeBuilder> DISTILLATION_RECIPES;
 
     static {
         CIRCUIT_ASSEMBLER_RECIPES = (new RecipeMap("circuit_assembler", 1, 6, 1, 1, 0, 1, 0, 0, 1, new AssemblerRecipeBuilder())).setSlotOverlay(false, false, GuiTextures.CIRCUIT_OVERLAY).setProgressBar(GuiTextures.PROGRESS_BAR_CIRCUIT, ProgressWidget.MoveType.HORIZONTAL);
         CLUSTER_MILL_RECIPES = (new RecipeMap("cluster_mill", 1, 1, 1, 1, 0, 0, 0, 0, 1, new SimpleRecipeBuilder())).setSlotOverlay(false, false, GuiTextures.BENDER_OVERLAY).setProgressBar(GuiTextures.PROGRESS_BAR_BENDING, ProgressWidget.MoveType.HORIZONTAL);
         ASSEMBLY_LINE_RECIPES = (new RecipeMap("assembly_line", 4, 15, 1, 1, 0, 0, 0, 12, 1, new SimpleRecipeBuilder())).setSlotOverlay(false, false, GuiTextures.MOLD_OVERLAY).setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, ProgressWidget.MoveType.HORIZONTAL);
         COKE_OVEN_RECIPES = new ArrayList();
+
+        DISTILLATION_RECIPES = new RecipeMap("distill_tower", 0, 0, 0, 1, 1, 1, 1, 11, 1, (new UniversalDistillationRecipeBuilder()).notOptimized());
     }
 }
