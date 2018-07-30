5974f87d956963780997bf103297f8d9846a5177
Bartz24 <bartz24games@gmail.com>
2018-07-30 15:31:23 -0400

    textures work with coke brick/reinforced glass

--- src/main/java/gregicadditions/item/GATransparentCasing.java
+++ src/main/java/gregicadditions/item/GATransparentCasing.java
@@ -5,6 +5,7 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -25,6 +26,11 @@ public class GATransparentCasing extends VariantBlock<GATransparentCasing.Casing
         return false;
     }
 
+
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.CUTOUT;
+    }
 
     public enum CasingType implements IStringSerializable {
 
