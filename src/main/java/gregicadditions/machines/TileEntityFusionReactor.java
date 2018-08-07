2e3874736f7c7af38a7151d3970275bdfed14678
Bartz24 <bartz24games@gmail.com>
2018-08-07 11:39:31 -0400

    Fusion handles energy correctly now

--- src/main/java/gregicadditions/machines/TileEntityFusionReactor.java
+++ src/main/java/gregicadditions/machines/TileEntityFusionReactor.java
@@ -1,34 +1,52 @@
 package gregicadditions.machines;
 
-import gregicadditions.item.GAMetaBlocks;
-import gregicadditions.item.GAMultiblockCasing;
-import gregicadditions.item.GATransparentCasing;
-import gregicadditions.recipes.GARecipeMaps;
+import gregicadditions.GATextures;
 import gregtech.api.GTValues;
+import gregtech.api.capability.IEnergyContainer;
+import gregtech.api.capability.impl.EnergyContainerHandler;
+import gregtech.api.capability.impl.EnergyContainerList;
+import gregtech.api.capability.impl.FluidTankList;
+import gregtech.api.capability.impl.ItemHandlerList;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.MetaTileEntityHolder;
 import gregtech.api.metatileentity.multiblock.MultiblockAbility;
 import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
 import gregtech.api.multiblock.BlockPattern;
 import gregtech.api.multiblock.FactoryBlockPattern;
+import gregtech.api.multiblock.PatternMatchContext;
 import gregtech.api.recipes.RecipeMaps;
 import gregtech.api.render.ICubeRenderer;
-import gregtech.api.render.Textures;
-import gregtech.common.blocks.*;
+import gregtech.api.util.GTUtility;
+import gregtech.common.blocks.BlockMachineCasing;
+import gregtech.common.blocks.BlockMultiblockCasing;
+import gregtech.common.blocks.BlockWireCoil;
+import gregtech.common.blocks.MetaBlocks;
 import gregtech.common.metatileentities.MetaTileEntities;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.text.*;
+import net.minecraftforge.fluids.IFluidTank;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+import net.minecraftforge.items.ItemStackHandler;
+
+import java.util.Collections;
+import java.util.List;
 
 import static gregtech.api.multiblock.BlockPattern.RelativeDirection.*;
 
 public class TileEntityFusionReactor extends RecipeMapMultiblockController {
     private final int tier;
+    private EnergyContainerList inputEnergyContainers;
 
     public TileEntityFusionReactor(String metaTileEntityId, int tier) {
         super(metaTileEntityId, RecipeMaps.FUSION_RECIPES);
         this.tier = tier;
         this.reinitializeStructurePattern();
+        this.energyContainer = new EnergyContainerHandler(this, Integer.MAX_VALUE, 0, 0, 0, 0){
+            public String getName() {
+                return "EnergyContainerInternal";
+            }
+        };
     }
 
     @Override
@@ -78,13 +96,15 @@ public class TileEntityFusionReactor extends RecipeMapMultiblockController {
                     }
                     return false;
                 })))
-                .where('#', (tile)->{return true;})
+                .where('#', (tile) -> {
+                    return true;
+                })
                 .build();
     }
 
     @Override
     public ICubeRenderer getBaseTexture() {
-        return Textures.SOLID_STEEL_CASING;
+        return GATextures.FUSION_TEXTURE;
     }
 
     protected IBlockState getCasingState() {
@@ -110,6 +130,75 @@ public class TileEntityFusionReactor extends RecipeMapMultiblockController {
             default:
                 return MetaBlocks.WIRE_COIL.getState(BlockWireCoil.CoilType.FUSION_COIL);
         }
+    }
+
+    private long getMaxEU() {
+        long val = 0;
+        List<IEnergyContainer> eConts = ObfuscationReflectionHelper.getPrivateValue(EnergyContainerList.class, this.inputEnergyContainers, "energyContainerList");
+        for (IEnergyContainer energyContainer : eConts) {
+            int tier = (int) (Math.pow(energyContainer.getEnergyCapacity() / 8L, .25D)) / 2;
+            val += (long) Math.pow(2, (tier - 6)) * 10000000;
+        }
+        return val;
+    }
+
+    protected void formStructure(PatternMatchContext context) {
+        long energyStored = this.energyContainer.getEnergyStored();
+        super.formStructure(context);
+        this.initializeAbilities();
+        ((EnergyContainerHandler) this.energyContainer).setEnergyStored(energyStored);
+    }
+
+    private void initializeAbilities() {
+        this.importItems = new ItemHandlerList(this.getAbilities(MultiblockAbility.IMPORT_ITEMS));
+        this.importFluids = new FluidTankList(this.getAbilities(MultiblockAbility.IMPORT_FLUIDS));
+        this.exportItems = new ItemHandlerList(this.getAbilities(MultiblockAbility.EXPORT_ITEMS));
+        this.exportFluids = new FluidTankList(this.getAbilities(MultiblockAbility.EXPORT_FLUIDS));
+        this.inputEnergyContainers = new EnergyContainerList(this.getAbilities(this.shouldUseEnergyOutputs() ? MultiblockAbility.OUTPUT_ENERGY : MultiblockAbility.INPUT_ENERGY));
+        this.energyContainer = new EnergyContainerHandler(this, getMaxEU(), 0, 0, 0, 0){
+            public String getName() {
+                return "EnergyContainerInternal";
+            }
+        };
+    }
+
+    protected void updateFormedValid() {
+        if(!getWorld().isRemote && this.inputEnergyContainers.getEnergyStored() > 0) {
+            long energyAdded = this.energyContainer.addEnergy(this.inputEnergyContainers.getEnergyStored());
+            if(energyAdded > 0)
+            this.inputEnergyContainers.addEnergy(-energyAdded);
+        }
+        this.recipeMapWorkable.updateWorkable();
+    }
+
+    protected void addDisplayText(List<ITextComponent> textList) {
+        if (!this.isStructureFormed()) {
+            textList.add((new TextComponentTranslation("gregtech.multiblock.invalid_structure", new Object[0])).setStyle((new Style()).setColor(TextFormatting.RED)));
+        }
+        if (this.isStructureFormed()) {
+            boolean isGenerator = this.shouldUseEnergyOutputs();
+            if (!this.recipeMapWorkable.isWorkingEnabled()) {
+                textList.add(new TextComponentTranslation("gregtech.multiblock.work_paused", new Object[0]));
+            } else if (this.recipeMapWorkable.isActive()) {
+                textList.add(new TextComponentTranslation("gregtech.multiblock.running", new Object[0]));
+                int currentProgress;
+                if (!isGenerator) {
+                    currentProgress = (int)(this.recipeMapWorkable.getProgressPercent() * 100.0D);
+                    textList.add(new TextComponentTranslation("gregtech.multiblock.progress", new Object[]{currentProgress}));
+                } else {
+                    currentProgress = -this.recipeMapWorkable.getRecipeEUt();
+                    textList.add(new TextComponentTranslation("gregtech.multiblock.generation_eu", new Object[]{currentProgress}));
+                }
+            } else {
+                textList.add(new TextComponentTranslation("gregtech.multiblock.idling", new Object[0]));
+            }
+
+            if (this.recipeMapWorkable.isHasNotEnoughEnergy()) {
+                textList.add((new TextComponentTranslation("gregtech.multiblock.not_enough_energy", new Object[0])).setStyle((new Style()).setColor(TextFormatting.RED)));
+            }
+        }
+
+        textList.add(new TextComponentString("EU: " + this.energyContainer.getEnergyStored() + " / " + this.energyContainer.getEnergyCapacity()));
     }
 }
 
