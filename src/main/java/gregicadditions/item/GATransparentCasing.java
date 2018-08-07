f0ea62f71319cf4b850f5c08dd59aaf1ecc936e2
Bartz24 <bartz24games@gmail.com>
2018-08-07 16:27:30 -0400

    fixed reinforced glass

--- src/main/java/gregicadditions/item/GATransparentCasing.java
+++ src/main/java/gregicadditions/item/GATransparentCasing.java
@@ -1,11 +1,14 @@
 package gregicadditions.item;
 
 import gregtech.common.blocks.VariantBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -38,6 +41,15 @@ public class GATransparentCasing extends VariantBlock<GATransparentCasing.Casing
     public boolean isOpaqueCube(IBlockState state)
     {
         return false;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side)
+    {
+        IBlockState iblockstate = blockAccess.getBlockState(pos.offset(side));
+        Block block = iblockstate.getBlock();
+
+        return block == this ? false : super.shouldSideBeRendered(blockState, blockAccess, pos, side);
     }
 
     public enum CasingType implements IStringSerializable {
