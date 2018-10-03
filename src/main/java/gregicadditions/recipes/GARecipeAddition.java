638542f1a5f1f73d13a858c6260854433c386d29
EmosewaPixel <awesomeggaming@gmail.com>
2018-10-03 12:00:51 +0200

    Revert "Temporarily Fix the Lapotron Crystal recipe"
    This reverts commit 2b1b58b05323deea9cb6f5e21b082f06542dd48d.

--- src/main/java/gregicadditions/recipes/GARecipeAddition.java
+++ src/main/java/gregicadditions/recipes/GARecipeAddition.java
@@ -101,6 +101,7 @@ public class GARecipeAddition {
     };
 
     private static final MaterialStack[] lapisLike = {
+            new MaterialStack(Materials.Lapis, 1),
             new MaterialStack(Materials.Lazurite, 1),
             new MaterialStack(Materials.Sodalite, 1)
     };
